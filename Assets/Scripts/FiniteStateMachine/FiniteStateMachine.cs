2d921ad21908730a7a5e6ed93da795422c925473
Fredrik Sy <fredrik.sy@icloud.com>
2020-01-07 20:20:02 +0100

    Add finite-state machine

---
+++ Assets/Scripts/FiniteStateMachine/FiniteStateMachine.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FiniteStateMachine : MonoBehaviour
+{
+    [SerializeField] private State m_DefaultState;
+    [SerializeField] private List<State> m_States;
+    private State m_CurrentState;
+
+    private void Start()
+    {
+        m_States.Add(m_DefaultState);
+
+        foreach (State state in m_States)
+        {
+            state.GameObject = gameObject;
+            state.Initialize();
+        }
+    }
+
+    private void Update()
+    {
+        if (m_States.Count == 0)
+            return;
+
+        if (m_CurrentState == null)
+            m_CurrentState = m_DefaultState;
+
+        if (m_CurrentState == null)
+            return;
+
+        int oldStateID = m_CurrentState.StateID;
+        int stateID = m_CurrentState.CheckTransitions();
+
+        if (stateID != oldStateID)
+        {
+            foreach (State state in m_States)
+            {
+                if (state.StateID == stateID)
+                {
+                    m_CurrentState.Exit();
+                    m_CurrentState = state;
+                    m_CurrentState.Enter();
+                }
+            }
+        }
+
+        m_CurrentState.Update();
+    }
+}
---
+++ Assets/Scripts/FiniteStateMachine/State.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public abstract class State : ScriptableObject
+{
+    public GameObject GameObject { get; set; }
+    public int StateID { get; protected set; }
+    public abstract int CheckTransitions();
+    public abstract void Enter();
+    public abstract void Exit();
+    public abstract void Initialize();
+    public abstract void Update();
+}
