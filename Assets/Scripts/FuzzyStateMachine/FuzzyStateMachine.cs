54a1cdc19a39432e1a756f5033cc12f166914c63
Fredrik Sy <fredrik.sy@icloud.com>
2020-01-08 22:29:43 +0100

    Add fuzzy-state machine

---
+++ Assets/Scripts/FuzzyStateMachine/FuzzyStateMachine.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace FuzzyStateMachine
+{
+    public class FuzzyStateMachine : MonoBehaviour
+    {
+        [SerializeField] private State[] m_States;
+        private bool[] m_ActivatedStates;
+
+        private void Start()
+        {
+            m_ActivatedStates = new bool[m_States.Length];
+
+            for (int i = 0; i < m_States.Length; i++)
+            {
+                m_States[i].GameObject = gameObject;
+                m_States[i].Initialize();
+            }
+        }
+
+        private void Update()
+        {
+            if (m_States.Length == 0)
+                return;
+
+            for (int i = 0; i < m_States.Length; i++)
+            {
+                if (m_States[i].CalculateActivation() > 0)
+                {
+                    if (!m_ActivatedStates[i])
+                    {
+                        m_ActivatedStates[i] = true;
+                        m_States[i].Enter();
+                    }
+
+                    m_States[i].Update();
+                }
+                else
+                {
+                    if (m_ActivatedStates[i])
+                    {
+                        m_ActivatedStates[i] = false;
+                        m_States[i].Exit();
+                    }
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            for (int i = 0; i < m_States.Length; i++)
+            {
+                m_States[i].Exit();
+                m_States[i].Initialize();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/FuzzyStateMachine/State.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace FuzzyStateMachine
+{
+    public abstract class State : ScriptableObject
+    {
+        public GameObject GameObject { get; set; }
+        public abstract float CalculateActivation();
+        public abstract void Enter();
+        public abstract void Exit();
+        public abstract void Initialize();
+        public abstract void Update();
+    }
+}
